825989ddd6ef45c8791ba9d7ea9fe9b4d87d88fd
Hannes Moser <hannes@impossiblearts.com>
2015-12-09 02:21:32 +0100

    feat: add CMS

--- 02-composer/index.php
+++ 02-composer/index.php
@@ -1,4 +1,7 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-phpinfo();
+
+$log = new Monolog\Logger( 'name' );
+$log->pushHandler( new Monolog\Handler\StreamHandler( 'app.log', Monolog\Logger::WARNING ) );
+$log->addWarning( 'Foo' );
